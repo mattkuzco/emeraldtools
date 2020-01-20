ef73ebd90a0914c11a4a91ed32b6b8771241c60e
Gigi <krillaxten@gmail.com>
2020-01-20 20:48:09 +0100

    Add sword and hoe to items

--- src/main/java/com/example/examplemod/ModItems.java
+++ src/main/java/com/example/examplemod/ModItems.java
@@ -7,6 +7,8 @@ public class ModItems
     public static final Item[] ITEMS = {
             new EmeraldItems("emerald_pickaxe", "pickaxe", 3),
             new EmeraldItems("emerald_axe", "axe", 3),
-            new EmeraldItems("emerald_shovel", "shovel", 3)
+            new EmeraldItems("emerald_shovel", "shovel", 3),
+            new EmeraldItems("emerald_hoe", "hoe", 3),
+            new EmeraldItems("emerald_sword", "sword", 3)
     };
 }
