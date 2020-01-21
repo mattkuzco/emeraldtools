ee317d876583fc7d953eed7c3b97a96c7cdb4458
Gigi <krillaxten@gmail.com>
2020-01-21 01:34:45 +0100

    Rename version

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -26,7 +26,7 @@ public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.1a";
 
     private static Logger logger;
 
@@ -42,8 +42,6 @@ public class ExampleMod
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-
-
     }
 
 
